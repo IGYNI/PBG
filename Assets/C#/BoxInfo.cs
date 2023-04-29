706bbd37c23679b88b93e88e5f0f207d3bed4fda
me <mwolf2380@gmail.com>
2023-04-29 10:19:03 +0300

    Remove

---
+++ Assets/C#/BoxInfo.cs
@@ -0,0 +1,13 @@
+using System.Drawing;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoxInfo : MonoBehaviour
+{
+    public UnityEngine.Color BoxMaterial;
+    
+
+
+}
